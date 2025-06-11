3c1906691c2dd28f89fb24a47233aa44165db4eb
svyat <sstndart@gmail.com>
2025-06-11 11:43:40 +0300

    vlm abliterated example

--- examples/lmstudio_vlm.py
+++ examples/lmstudio_vlm.py
@@ -1,9 +1,10 @@
 import lmstudio as lms
 
 with lms.Client('95.165.10.219:1234') as client:
-    model = client.llm.model('gemma-3-27b-it-qat', ttl=30)
-    image_path = 'examples/downloads/enhel_w.png'
-    # image_path = "examples/downloads/ump_9.jpg"
+    # model = client.llm.model('gemma-3-27b-it-qat', ttl=30)
+    model = client.llm.model('gemma-3-27b-it-abliterated', ttl=30)
+
+    image_path = 'examples/downloads/camelliya.jpg'
     image_handle = client.prepare_image(image_path)
 
     chat = lms.Chat()
